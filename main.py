0a526587dcb1ee9d2a4accfaf96d851355ee6d7c
Naman Tyagi <144577127+Noah-1001001@users.noreply.github.com>
2026-06-05 15:25:21 +0530

    Refactor main.py to send weather alerts via Twilio
    Updated the script to use Twilio for weather alerts and removed email functionality.

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime as dt
-import pandas as pd
-import random
-import smtplib
+from twilio.rest import Client
+import requests
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("namantyagidpsg@gmail.com")
-MY_PASSWORD = os.environ.get("skaxpovwlsprhbep")
 
-df = pd.read_csv("./birthdays.csv")
-now = dt.now()
+WEATHER_API_KEY = os.environ.get("OWM_API_KEY")
+LAT = 28.6650
+LON = 77.4485
+ACCOUNT_SID = "ACf511788dffdebc770d7afd6f37bd1418"
+AUTH_TOKEN = os.environ.get("TW_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = "+19346491425"
+MY_PHONE_NUMBER = os.environ.get("MY_PN")
 
-receiver_data = df[(df['day'] == now.day) & (df['month'] == now.month)]
+parameters = {
+    "lat": LAT,
+    "lon": LON,
+    "appid": WEATHER_API_KEY,
+    "cnt": 4,
+    "units": "metric"
+}
+
+response = requests.get(url="http://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+count = data["cnt"]
+
+timestamp_codes = [data["list"][i]["weather"][0]["id"] for i in range(0, count)]
 
 
-if not receiver_data.empty:
-    receiver_name = receiver_data.iloc[0, 0]
-    receiver_email = receiver_data.iloc[0, 1]
+if any(code < 600 for code in timestamp_codes):
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-    random_number = random.randint(1, 3)
-    with open(f"./letter_templates/letter_{random_number}.txt", mode='r') as file:
-        chosen_template = file.read()
-        birthday_letter = chosen_template.replace("[NAME]", str(receiver_name))
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body="It's Going to Rain Today, Bring out the Umbrellas.🌧️☔️",
+        to='whatsapp:+919871871250'
+    )
+    print(message.status)
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, PASSWORD)
-        connection.sendmail(MY_EMAIL, receiver_email, msg=f"Subject:Happy Birthday!\n\n{birthday_letter}")
