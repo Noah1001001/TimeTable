7cd1b92a4353c33a6cf8ef8e3ff77934b6e0bb71
Naman_Tyagi1001001 <102236255+Noah1001001@users.noreply.github.com>
2026-09-13 11:18:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,13 @@ chrome_options.add_experimental_option('prefs', {
     "credentials_enable_service": False, 
     "profile.password_manager_enabled": False 
 })
-driver = uc.Chrome(options=chrome_options, version_main=152)
 
 chrome_options.add_experimental_option('prefs', {
     "credentials_enable_service": False,
     "profile.password_manager_enabled": False
 })
 
-driver = uc.Chrome(options=chrome_options)
+driver = uc.Chrome(options=chrome_options, version_main=152)
 driver.get('https://s.amizone.net/')
 time.sleep(2)
 
