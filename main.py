2fe4c970ad6086ae9811a0f4c5ea4fde9c5cee0b
Naman_Tyagi1001001 <102236255+Noah1001001@users.noreply.github.com>
2026-09-21 23:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ CHAT_ID = os.environ["CHAT_ID"]
 
 # ------------------------ SETTING UP CHROME -------------------------
 chrome_options = uc.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--disable-gpu")
 chrome_options.add_experimental_option('prefs', {
     "credentials_enable_service": False,
     "profile.password_manager_enabled": False
